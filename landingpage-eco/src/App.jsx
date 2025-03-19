795ff36eaefe90513586a1799580d93f668b2c18
douglasnou <dodonou3@gmail.com>
2025-03-19 16:11:40 -0300

    implementando google analytics

--- landingpage-eco/src/App.jsx
+++ landingpage-eco/src/App.jsx
@@ -11,21 +11,9 @@ import { Reasons } from "./sections/Reasons";
 import { SecondBanner } from "./sections/SecondBanner";
 import { SecondDetail } from "./sections/SecondDetail";
 import { Security } from "./sections/Security";
-import ReactGA from "react-ga4";
 import "../src/styles/index.scss";
-import { useEffect } from "react";
 
 function App() {
-
-  if(!import.meta.env.VITE_GA_KEY){
-    console.log("Chave não encontrada.")
-  }
-
-  useEffect(() => {
-    ReactGA.initialize(import.meta.env.VITE_GA_KEY);
-
-    ReactGA.send("pageview");
-  }, [])
 
   return (
     <>
