06ac78edbfb3a4dd38d3c07d36c36f4a678729b7
douglasnou <dodonou3@gmail.com>
2025-10-08 00:45:43 -0300

    mudança no link para whatsapp

--- landingpage-eco/src/sections/KnowMore/index.jsx
+++ landingpage-eco/src/sections/KnowMore/index.jsx
@@ -38,7 +38,7 @@ export const KnowMore = () => {
                     <img src={Logo} />
                     <img src={Logo100} />
                 </div>
-                <a target="blank" href="https://wa.me/5599991971477?text=Ol%C3%A1%2C+gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+a+Eco+Casa+Coroat%C3%A1%21%F0%9F%8F%A0%E2%9C%A8"
+                <a target="blank" href="https://wa.me/5598985243214?text=Olá%2C%20gostaria%20de%20mais%20informações%20sobre%20a%20Eco%20Casa%20Lago%20da%20Pedra!"
                 >
                     <div>
                         <img src={WhatsApp} />
